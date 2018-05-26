2cf0605338b5ce07c6bf5cc1cddcb5248f577d4c
Samuel Seyoum <samiseyoum@aol.com>
2018-05-26 22:13:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function happyHolidayTo (holiday, name) {
 
 
 function holidayCountdown(holiday, days) {
-  return ("It's 6 days until, Independence Day")
+  return ("It's 20 days until Mother's Day!")
 }
