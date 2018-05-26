f33427cb7dc227333cc2795b05846555e313cc0e
Samuel Seyoum <samiseyoum@aol.com>
2018-05-26 21:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+happyHolidays ("Happy holidays");
+
+function happyHolidaysTo (name) {
+  return ("Happy holidays, you!") 
+}
+
+
+function happyHolidayTo (holiday, name) {
+  retrun ("holiday" + "name!")
+}
+
