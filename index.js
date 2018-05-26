8a3ec7221d74f8deb5c7f8be3e1679cb8298b085
Samuel Seyoum <samiseyoum@aol.com>
2018-05-26 22:12:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function happyHolidayTo (holiday, name) {
   return ("Happy Independence Day, you!")
 }
 
+
+function holidayCountdown(holiday, days) {
+  return ("It's 6 days until, Independence Day")
+}
